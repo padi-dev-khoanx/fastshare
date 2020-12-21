57634752255acfbb2d9ff4d17c1369832daf300c
padi-dev-khoanx <73452361+padi-dev-khoanx@users.noreply.github.com>
2020-12-21 15:50:59 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,55 +8,46 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
-    <style>
-        .progress {
-            position:relative;
-            width:100%;
-            height: 20px;
-        }
-        .bar {
-            background-color: #498C25;
-            width:0%;
-            height:20px;
-        }
-        .percent {
-            position:absolute;
-            display:inline-block;
-            margin: auto;
-            text-align: center;
-            color: #fff;
-            z-index: 99;
-            width: 100%;
-            height: 20px;
-        }
-   </style>
 </head>
 <body>
-<!-- partial:index.partial.html -->
 <div class="frame">
-    <form action="{{ url('upload_file') }}" enctype="multipart/form-data" method="POST">
-    @csrf
     <div class="center">
-        <div class="bar"></div>
-        <div class="title">Kéo thả file vào đây để tải lên</div>
-        <div class="dropzone">
-            <div class="content">
-                <img src="{{asset('/img/upload.svg')}}" class="upload">
-                <span class="filename"></span>
-                <input type="file" class="input" name="file_share" required>
+        <div class="upload-content">
+            <form action="{{ url('upload_file') }}" enctype="multipart/form-data" method="POST">
+                @csrf
+                <div class="title">Kéo thả file vào đây để tải lên</div>
+                <div class="sub-title">Tải lên file lên tới 100MB</div>
+                <div class="progress">
+                    <div class="bar"></div >
+                    <div class="percent">0%</div >
+                </div>
+                <div class="dropzone">
+                    <div class="content">
+                        <img src="{{asset('/img/upload.svg')}}" class="upload">
+                        <span class="filename"></span>
+                        <input type="file" class="input" name="file_share" required>
+                    </div>
+                </div>
+                <img src="{{asset('/img/syncing.svg')}}" class="syncing">
+                <img src="{{asset('/img/checkmark.svg')}}" class="done">
+                <label class="upload-btn" for="submit">
+                    Tải lên
+                </label>
+                <input type="submit" id="submit" hidden>
+            </form>
+        </div>
+        <div class="show-link">
+            <div class="title">Đường dẫn chia sẻ</div>
+            <div class="link-copy">
+                <input type="text" class="link-input" id="link-input">
+                <div id="link-value"></div>
+                <div class="copy-btn" onclick="copyToClipboard('#link-value')">
+                    <span id="copy-text"> Sao chép liên kết</span>
+                </div>
             </div>
         </div>
-        <img src="{{asset('/img/syncing.svg')}}" class="syncing">
-        <img src="{{asset('/img/checkmark.svg')}}" class="done">
-        <div class="progress">
-            <div class="bar"></div >
-            <div class="percent">0%</div >
-        </div>
-        <button class="upload-btn" type="submit">Tải lên</button>
     </div>
-    </form>
 </div>
-<!-- partial -->
 
 <script type="text/javascript">
     var droppedFiles = false;
@@ -66,9 +57,11 @@
     var uploading = false;
     var $syncing = $('.syncing');
     var $done = $('.done');
-    var $bar = $('.bar');
-    var timeOut;
-
+    var SITEURL = window.location.href;
+    var bar = $('.bar');
+    var percent = $('.percent');
+    var $upload_content = $('.upload-content');
+    var $show_link = $('.show-link')
     $dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -99,25 +92,12 @@
     function startUpload() {
         if (!uploading && fileName != '' ) {
             uploading = true;
-            $button.html('Đang tải lên...');
-            $dropzone.fadeOut();
-            $syncing.addClass('active');
-            $done.addClass('active');
-            $bar.addClass('active');
-            // timeoutID = window.setTimeout(showDone, 3200);
         }
     }
 
-    // function showDone() {
-    //     $button.html('Xong');
-    // }
-
-    var SITEURL = window.location.href;
     $(function() {
         $(document).ready(function()
         {
-            var bar = $('.bar');
-            var percent = $('.percent');
             $('form').ajaxForm({
                 beforeSend: function() {
                     var percentVal = '0%';
@@ -125,21 +105,48 @@
                     percent.html(percentVal);
                 },
                 uploadProgress: function(event, position, total, percentComplete) {
+                    window.onbeforeunload = function ()
+                    {
+                        return "";
+                    };
                     var percentVal = percentComplete + '%';
+                    uploading = true;
                     bar.width(percentVal)
                     percent.html(percentVal);
+                    $button.html('Đang tải lên...');
+                    $syncing.addClass('active');
+                    $dropzone.fadeOut();
                 },
                 complete: function(xhr) {
                     // alert('Link download: ' + SITEURL + 'upload/' + fileName);
                     // window.location.href = SITEURL;
+                    window.onbeforeunload = function () {}
                     $button.html('Xong');
-                    value = 'ahihi'
-                    $temp.val(value).select();
-                    document.execCommand("copy");
+                    $syncing.removeClass('active');
+                    $done.addClass('active');
+                    setTimeout(function(){
+                        $upload_content.fadeOut();
+                        $show_link.fadeIn(1500);
+                    }, 2000);
+                    $('.link-input').val(SITEURL + 'upload/' + fileName);
+                    $('#link-value').html(SITEURL + 'upload/' + fileName);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status);
+                    alert(thrownError);
                 }
             });
         });
     });
+
+    function copyToClipboard(element) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(element).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+        $("#copy-text").text("Đã sao chép liên kết");
+    }
 </script>
 
 </body>
